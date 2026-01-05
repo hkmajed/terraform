887ae606ed4a29fa1b7beee941f5c6ce70eebd1b
Hussein Majed <hkmajed@hotmail.com>
2026-01-04 21:58:21 -0500

    Terraform type constraints

---
+++ 06-Type Constraints/provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.16.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.allowed_locations[0]
+
+  default_tags {
+    tags = {
+      Project     = "Terraform-AWS"
+      Topic       = "Type-Constraints"
+      ManagedBy   = "Terraform"
+      Environment = var.environment
+    }
+  }
+}
